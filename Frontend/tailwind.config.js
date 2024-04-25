570f949af06da3ec9cc5f8c2626ec1ffcbe20597
mudasir04 <mudasir04@gmail.com>
2024-04-25 23:23:16 +0530

    Frontend Done!!

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("daisyui")],
+export default {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    darkMode: "class",
+    theme: {
+        extend: {},
+    },
+    plugins: [require("daisyui")],
 };
